c7d002091aaf1e8d8095defdcc3e3e3ad2fca020
andreson.csilva <andreson.csilva@CAS555589W0736.senacsp.edu.br>
2017-03-24 20:31:20 -0300

    TestesEmAula

---
+++ src/jogodavelha1/JogoDaVelha1.java
@@ -0,0 +1,21 @@
+
+package jogodavelha1;
+
+
+public class JogoDaVelha1 {
+
+    
+    public static void main(String[] args) {
+        String centro;
+        centro = " X ";
+        
+        System.out.println("    |    |    ");
+        System.out.println("----+----+----");
+        System.out.println("    |" + centro + " |    ");
+        System.out.println("----+----+----");
+        System.out.println("    |    |    ");
+        
+        
+    }
+    
+}
