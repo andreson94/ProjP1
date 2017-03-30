879f113346e74a6a502e1e99b99422d7529104c9
andreson.csilva <andreson.csilva@CAS493934W10B22.senacsp.edu.br>
2017-03-30 18:32:29 -0300

    exe6completo

--- ALPI/src/ADO/Exercicio6.java
+++ ALPI/src/ADO/Exercicio6.java
@@ -15,15 +15,59 @@ f) o perímetro do retângulo que tem lados A e B.
         Scanner leitor = new Scanner(System.in);
         int la, lb, lc;
         double areatr, areac, areat, areaq ,arrear, perimetror;
-   
+            
+            System.out.println("Ára do TR\n");
             System.out.print("Informe o lado a: ");
             la = Integer.parseInt(leitor.nextLine());
             System.out.print("Informe o lado b: ");
             lb = Integer.parseInt(leitor.nextLine());
-            System.out.println("Informe o lado c:");
-            lc = Integer.parseInt(leitor.nextLine());
+            lc = (int) (Math.pow(la, 2) + Math.pow(lb, 2)); 
             areatr = (la * lc)/2;
-            System.out.println("Area do triangulo Retangulo é: " + areatr );
+            System.out.printf("Área do triangulo Retangulo é: %.0f\nbase:%d\nAltura: %d\n ",areatr,la,lb );
+            System.out.println("");
 
+            System.out.println("Área do círculo\n");
+            double r = 3.14159, a;
+            a = Math.PI * r * r;
+            System.out.printf("Área do circulo: %f \n\n",a);
+            
+            
+            System.out.println("Área do Trapézio\n");
+            int b,B,alt,area;
+            System.out.print("Informe a Base Maior: ");
+            B = Integer.parseInt(leitor.nextLine());
+            System.out.print("Informe a Base menor: ");
+            b = Integer.parseInt(leitor.nextLine());
+            System.out.print("Informe a Altura: ");
+            alt = Integer.parseInt(leitor.nextLine());
+            area = (int) ((( B * b )/2 *alt));
+            System.out.println("Area do Trapézio:\n" + area);
+            
+            System.out.println("Área do Quadrado\n");
+            int ladob,areaquadrado;
+            System.out.print("informe o Lado: ");
+            ladob = Integer.parseInt(leitor.nextLine());
+            areaquadrado = (int) Math.pow(ladob,2);
+            System.out.printf("Área do Quadrado: %d\n\n",areaquadrado);
+            
+            System.out.println("Área do Retangulo\n");
+            int aretangulo;
+            System.out.print("Informe Base: ");
+            b = Integer.parseInt(leitor.nextLine());
+            System.out.print("Informe Altura:");
+            alt = Integer.parseInt(leitor.nextLine());
+            aretangulo = b * alt;
+            System.out.println("Área do retangulo: " + aretangulo);
+            System.out.println("");
+            
+            System.out.println("Perimetro Retangulo\n");
+            int p;
+            System.out.print("Informe o lado A: ");
+            la = Integer.parseInt(leitor.nextLine());
+            System.out.print("Informe o lado B: ");
+            lb = Integer.parseInt(leitor.nextLine());
+            p = 2*(la + lb);
+            System.out.printf("Perimetro:%d\n",p);
+            
     }
 }
