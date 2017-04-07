11d13030851c6894b88cbe7e110c8b9d755af8ef
Andreson <andreson.dc.silva@gmail.com>
2017-04-07 00:31:38 -0300

    exe4fim

--- ALPI/src/ADOII/exe4.java
+++ ALPI/src/ADOII/exe4.java
@@ -1,14 +1,59 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ADOII;
 
-/**
- *
- * @author Andreson
- */
+import javax.swing.JOptionPane;
+
 public class exe4 {
+    public static void main(String[] args) {
+                int mes,ano;
+                JOptionPane.showMessageDialog(null, "CALENDARIO");
+                mes =Integer.parseInt(JOptionPane.showInputDialog(null,"Mês:"));
+                ano =Integer.parseInt(JOptionPane.showInputDialog(null,"Ano:"));
+        
+           
+            switch (mes) {
+                case 1:
+                    JOptionPane.showMessageDialog(null,"Janeiro, tem 31 dias");
+                    break;
+                case 2:
+                    JOptionPane.showMessageDialog(null,"Fevereiro, tem 28 dias");
+                    break;
+                case 3:
+                    JOptionPane.showMessageDialog(null,"Março, tem 31 dias");
+                    break;
+                case 4:
+                    JOptionPane.showMessageDialog(null,"Abril, tem 30 dias");
+                    break;
+                case 5:
+                    JOptionPane.showMessageDialog(null,"Maio, tem 31 dias");
+                    break;
+                case 6:
+                    JOptionPane.showMessageDialog(null,"Junho , tem 30 dias");
+                    break;
+                case 7:
+                    JOptionPane.showMessageDialog(null,"Junlho, tem 31 dias");
+                    break;
+                case 8:
+                    JOptionPane.showMessageDialog(null,"Agosto, tem 31 dias");
+                    break;
+                case 9:
+                    JOptionPane.showMessageDialog(null,"Setembro, tem 30 dias");
+                    break;
+                case 10:
+                    JOptionPane.showMessageDialog(null,"Outubro, tem 31 dias");
+                    break;
+                case 11:
+                    JOptionPane.showMessageDialog(null,"Novembro, tem 30 dias");
+                    break;
+                case 12:
+                    JOptionPane.showMessageDialog(null,"Dezembro, tem 31 dias");
+                    break;
+                default:
+                    JOptionPane.showMessageDialog(null,"Mês Invalido"); 
+                    break;
+            }
+        }
+    }
+  
     
-}
+    
+
