89226df43d76d50776b1441c67a9bb4cf38d36ad
Andreson <andreson.dc.silva@gmail.com>
2017-04-07 15:51:56 -0300

    exe8

--- ALPI/src/ADOII/exe8.java
+++ ALPI/src/ADOII/exe8.java
@@ -10,6 +10,51 @@ public class exe8 {
     public static void main(String[] args) {
     
         Scanner leitor = new Scanner(System.in);
-        
+        double anuidade,multa = 0,juros,juros2,r;
+        int mes; 
+        System.out.println("digite o mes:");
+        mes= Integer.parseInt(leitor.nextLine());
+        System.out.print("Digite o Valor da anuidade:");
+        anuidade = Double.parseDouble(leitor.nextLine());
+        juros =(anuidade * 5)/100; 
+        juros2 = ((anuidade + juros)*5)/100;
+        switch (mes){
+            case 1:
+                multa = anuidade; 
+                System.out.printf("Janeiro\nAnuidade: R$%f\nValor da Multa: R$ %f\n",anuidade,multa);
+            case 2:
+                multa = anuidade + juros;
+                System.out.printf("Janeiro\nAnuidade: R$%.2f\nValor da Multa: R$%.2f\n",anuidade,multa);
+            case 3:
+                multa = anuidade + juros + juros2;
+                System.out.printf("Janeiro\nAnuidade: R$%.2f\nValor da Multa: R$%.2f\n",anuidade,multa);
+            case 4:
+                r = multa + anuidade + juros + juros2; 
+                System.out.print("Janeiro\nAnuidade: R$%.2f\nValor da Multa: R$%.2f\n");
+            case 5:
+                multa = (((anuidade + juros + juros2)*5)/100)*5/100; 
+                System.out.print("Janeiro\nAnuidade: R$%.2f\nValor da Multa: R$%.2f\n");
+            case 6:
+                multa = anuidade + juros + juros2;
+                System.out.print("Janeiro\nAnuidade: R$%.2f\nValor da Multa: R$%.2f\n");
+            case 7:
+                multa = anuidade; 
+                System.out.print("Janeiro\nAnuidade: R$%.2f\nValor da Multa: R$%.2f\n");
+            case 8:
+                multa = anuidade + juros;
+                System.out.print("Janeiro\nAnuidade: R$%.2f\nValor da Multa: R$%.2f\n");
+            case 9:
+                multa = anuidade + juros + juros2;
+                System.out.print("Janeiro\nAnuidade: R$%.2f\nValor da Multa: R$%.2f\n");
+            case 10:
+                multa = anuidade; 
+                System.out.print("Janeiro\nAnuidade: R$%.2f\nValor da Multa: R$%.2f\n");
+            case 11:
+                multa = anuidade + juros;
+                System.out.print("Janeiro\nAnuidade: R$%.2f\nValor da Multa: R$%.2f\n");
+            case 12:
+                multa = anuidade + juros + juros2;
+                System.out.print("Janeiro\nAnuidade: R$%.2f\nValor da Multa: R$%.2f\n");        
+        }
     }
 }
