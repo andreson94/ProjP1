7a0d4f953d71960c50bf72b4398e899802940ade
Andreson <andreson.dc.silva@gmail.com>
2017-04-07 00:41:25 -0300

    exe5

---
+++ ALPI/src/ADOII/exe5.java
@@ -0,0 +1,35 @@
+package ADOII;
+
+import javax.swing.JOptionPane;
+
+public class exe5 {
+    public static void main(String[] args) {
+        String mes;
+                JOptionPane.showMessageDialog(null, "CALENDARIO TEMPORADAS");
+                mes =JOptionPane.showInputDialog(null,"Mês:");
+                mes = mes.toLowerCase();
+           
+            switch (mes) {
+                case "janeiro":
+                    JOptionPane.showMessageDialog(null,"Alta Temporada");
+                    break;
+                case "fevereiro":
+                    JOptionPane.showMessageDialog(null,"Alta Temporada");
+                    break;
+                case "junho":
+                    JOptionPane.showMessageDialog(null,"Alta Temporada");
+                    break;
+                case "julho":
+                    JOptionPane.showMessageDialog(null,"Alta Temporada");
+                    break;
+                case "dezembro":
+                    JOptionPane.showMessageDialog(null,"Alta Temporada");
+                    break;
+                default:
+                    JOptionPane.showMessageDialog(null,"Baixa Temporada"); 
+                    break;
+            }
+        }
+    }
+  
+    
