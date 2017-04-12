6b84cec8fc62a48202f31fb23f15cfa252dc46ba
andreson.csilva <andreson.csilva@CAS555589W0736.senacsp.edu.br>
2017-04-11 22:43:03 -0300

    pi

--- PI-I/src/EstruturaDeRepeticao/SomaSeries2.java
+++ PI-I/src/EstruturaDeRepeticao/SomaSeries2.java
@@ -1,9 +1,9 @@
 package EstruturaDeRepeticao;
 public class SomaSeries2 {
     public static void main(String[] args) {
-     double n = 1/1, soma=0;
+     double n = 1, soma=0;
        
-        for(int i = 1/1; i <= 10000; i++){
+        for(int i = 1; i <= 10000; i++){
            soma = soma + (1/n);
            n = n+2;
         }
